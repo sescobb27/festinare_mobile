70e0a7c525ea05ef4e99524856181ac173497134
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-27 20:19:20 -0500

    Gulp inject source code directly into index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
@@ -6,6 +8,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var inject = require('gulp-inject');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -49,4 +52,17 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('index', function () {
+  var target = gulp.src('./www/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['./www/js/**/*.js', './www/css/**/*.css']);
+
+  return target
+    .pipe(inject(sources, {
+      read: false,
+      relative: true
+    }))
+    .pipe(gulp.dest('./www'));
 });
