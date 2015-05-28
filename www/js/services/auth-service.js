e53a53b52eea0519790b8e31c940f2b15719ea4c
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-27 19:48:50 -0500

    Auth Service

---
+++ www/js/services/auth-service.js
@@ -0,0 +1,80 @@
+'use strict';
+
+angular.module('festinare_mobile')
+  .factory('AuthService', function ($rootScope, SessionService, $resource, UserService, $q) {
+    var AuthService = this;
+    var user_promise = null;
+    var client;
+    var subscriptors = [];
+
+    if(SessionService.getCurrentSession()) {
+      user_promise = UserService.get().then(function (res) {
+        console.log('CLIENT: ', res.client);
+        client = res.client;
+        notify(client);
+      });
+    } else {
+      user_promise = $q(function (resolve, reject) { reject(); });
+    }
+
+    $rootScope.$on('logout', function () {
+      AuthService.logout();
+    });
+
+    AuthService.login = function(credentials) {
+      return UserService.login(credentials).then(function (res) {
+        SessionService.addSession(res);
+        user_promise = UserService.get().then(function (res) {
+          client = res.client;
+          notify(client);
+          return client;
+        });
+        return user_promise;
+      });
+    };
+
+    AuthService.register = function(credentials) {
+
+    };
+
+    AuthService.logout = function() {
+      return UserService.logout();
+    };
+
+    // TODO
+    AuthService.forgotPassword = function(email) {};
+    // TODO
+    AuthService.resetPassword = function(token, password) {};
+    // TODO
+    AuthService.updatePassword = function(oldPassword, newPassword, userId) {};
+
+    AuthService.isLoggedIn = function () {
+      return user_promise.then(function () {
+        return !!client;
+      }).catch(function () {
+        return false;
+      });
+    };
+
+    AuthService.getCurrentUser = function() {
+      return user_promise.then(function () {
+        return client;
+      });
+    };
+
+    AuthService.subscribe = function(subscriptor) {
+      return subscriptors.push(subscriptor) - 1;
+    };
+
+    AuthService.unsuscribe = function(index) {
+      subscriptors.splice(index, 1);
+    };
+
+    var notify = function(user) {
+      angular.forEach(subscriptors, function (subscriptor) {
+        subscriptor.notify(user);
+      });
+    };
+
+    return AuthService;
+  });
