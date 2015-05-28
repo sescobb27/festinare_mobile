43127d7977a3aa028a9b05d608977be2ab03b54a
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-05-28 00:30:39 -0500

    adding angular resource, google roboto font and session service

---
+++ www/js/services/session-service.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('festinare_mobile')
+  .service('SessionService', function () {
+
+    var SessionService = this;
+
+    SessionService.addSession = function(data) {
+
+    };
+
+    SessionService.removeCurrentSession = function() {
+
+    };
+
+    SessionService.getCurrentSession = function() {
+
+    };
+  });
