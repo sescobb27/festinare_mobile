70fef52750549c41814375a3a4a26e3f61a11410
sescobb27 <s.escob.b.27@gmail.com>
2015-06-29 00:43:53 -0500

    [improve] improve ux/ui

--- www/js/components/user/dashboard/dashboard-controller.js
+++ www/js/components/user/dashboard/dashboard-controller.js
@@ -8,7 +8,7 @@ angular.module('festinare_mobile')
 
     var showLoading = function () {
       $ionicLoading.show({
-        template: 'loading'
+        template: '<ion-spinner></ion-spinner>'
       });
     };
 
